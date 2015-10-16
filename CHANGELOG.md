bddd52affe369b78dea232ec178683e58c151d2e
jclo <jc006@wanadoo.fr>
2015-10-16 12:10:28 +0200

    Made some slight enhancements.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,12 @@
 ### HEAD
 
-### 0.1.x (September 29, 2015)
+### 0.1.1 (October 16, 2015)
 
   * Added a CHANGELOG.md file,
+  * Modified the test file to use both 'should' and 'expect',
+  * Completed .eslintrc stylistic rules.
 
 
 ### 0.1.0 (September 29, 2015)
 
-* Initial build
+* Initial build.
--- test/main.js
+++ test/main.js
@@ -1,22 +1,22 @@
 /* global describe, it */
 var should = require('chai').should();
+var expect = require('chai').expect;
 var umdlib = require('../index.js');
 
 describe('UMDLib', function() {
 
   // Test the first 'method'.
-  describe('getArray', function() {
-    it ('Should be an array.', function() {
+  describe('The method getArray()', function() {
+    it ('Should return an array.', function() {
       var a = umdlib.getArray();
       a.should.be.a('array');
     });
   });
 
   // Test the second 'method'.
-  describe('getString', function() {
-    it ('Should be a string.', function() {
-      var s = umdlib.getString();
-      s.should.be.a('string');
+  describe('The method getString()', function() {
+    it ('Expects to return a string.', function() {
+      expect(umdlib.getString()).to.be.a('string');
     });
   });
 
